49723857c676314b38196c7eac1c95e4d3a5bc57
abvictor <victor.amaralb@hotmail.com>
2023-12-02 17:03:37 -0300

    feat: Add delete, list and update user functions

--- src/controllers/user/controllerUserRegister.ts
+++ src/controllers/user/controllerUserRegister.ts
@@ -6,6 +6,11 @@ const prisma = new PrismaClient();
 export class UserController {
   async create(req: Request, res: Response) {
     const { id, name, email, password, phone, isDev, github, linkedIn } = req.body;
+
+    if(!name || !email || !password || !phone || !isDev || !github || !linkedIn){
+      res.status(400).json({message: "All the fields are required"})
+      return
+    }
     try {
       const newUser = await prisma.user.create({
         data: {
@@ -19,10 +24,60 @@ export class UserController {
           linkedIn,
         },
       });
+      
       res.status(201).json({ message: 'User created' });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ message: 'An error occurred during user creation' });
+      res.status(400).json({ message: 'An error occurred during user creation' });
+    }
+  }
+
+
+  async list(req: Request, res: Response){
+    try {
+      const listUsers = await prisma.user.findMany({})
+        res.status(200).json({ listUsers });
+    } catch (error) {
+      
+    }
+  }
+
+  async update(req: Request, res: Response){
+    try {
+      const { name, email, password, phone, isDev, github, linkedIn } = req.body;
+      const updatedUser = await prisma.user.update({
+        where: {
+          id: parseInt(req.params.id)
+        },
+        data: {
+          name,
+          email,
+          password,
+          phone,
+          isDev,
+          github,
+          linkedIn,
+        }
+      })
+      res.status(200).json({updatedUser})
+    } catch (error) {
+
+      res.status(400).json({message: `Failed to update user ${req.params.id}`})
+      
+    }
+  }
+
+  async delete(req: Request, res: Response){
+    try {
+      await prisma.user.delete({
+        where: {
+          id: parseInt(req.params.id)
+        }
+      })
+      res.status(200).json({message: `Deleted user ${req.params.id} with success`})
+    } catch (error) {
+        res.status(400).json({message: `Failed to delete user ${req.params.id}`})
     }
   }
 }
+
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -5,6 +5,12 @@ const router = Router ();
 const userController = new UserController()
 
 
+router.get('/user', userController.list) 
 router.post('/user', userController.create)
+router.delete('/user/:id', userController.delete)
+router.put('/user/:id', userController.update)
+
+
+
 
 export default router;

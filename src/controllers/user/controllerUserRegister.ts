635d8c74ea5e9d4dabd721c000e9d8a01ba33a1a
abvictor <victor.amaralb@hotmail.com>
2023-12-02 17:34:11 -0300

    feat: add e-mail already in use validation

--- src/controllers/user/controllerUserRegister.ts
+++ src/controllers/user/controllerUserRegister.ts
@@ -4,14 +4,26 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export class UserController {
+
   async create(req: Request, res: Response) {
     const { id, name, email, password, phone, isDev, github, linkedIn } = req.body;
 
-    if(!name || !email || !password || !phone || !isDev || !github || !linkedIn){
-      res.status(400).json({message: "All the fields are required"})
-      return
+    if (!name || !email || !password || !phone || !isDev || !github || !linkedIn) {
+      res.status(400).json({ message: "All the fields are required" });
+      return;
     }
     try {
+      const existingUser = await prisma.user.findUnique({
+        where: {
+          email: email,
+        },
+      });
+  
+      if (existingUser) {
+        res.status(400).json({ message: "Email is already in use" });
+        return;
+      }
+
       const newUser = await prisma.user.create({
         data: {
           id,
@@ -24,7 +36,7 @@ export class UserController {
           linkedIn,
         },
       });
-      
+             
       res.status(201).json({ message: 'User created' });
     } catch (error) {
       console.error(error);
@@ -37,8 +49,9 @@ export class UserController {
     try {
       const listUsers = await prisma.user.findMany({})
         res.status(200).json({ listUsers });
+        return
     } catch (error) {
-      
+      res.status(500).json({ message: "It wasn't possible list the users" });
     }
   }
 
@@ -63,7 +76,6 @@ export class UserController {
     } catch (error) {
 
       res.status(400).json({message: `Failed to update user ${req.params.id}`})
-      
     }
   }
 

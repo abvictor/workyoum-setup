fcd9d9ffb89a3dd69129676a84af5bace84a0e04
abvictor <victor.amaralb@hotmail.com>
2023-12-02 13:53:22 -0300

    feat: project setup

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import * as dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors' 
+import router from './router/routes'
+
+
+dotenv.config()
+
+const app = express(); 
+
+app.use(cors()); 
+app.use(express.json())
+app.use(router)
+app.listen(5000, () => console.log(`server running on ${process.env.PORT}`))
+
+module.exports = app
